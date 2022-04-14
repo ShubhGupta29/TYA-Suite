47c2bc4e279fdcba1524f6932a3242ffd65e17ff
Shubh Gupta <shubh.gupta.0797@gmail.com>
2022-04-14 16:01:16 +0530

    Sign-Up Page2

--- src/app/tya/User-landing-Page/sign-up-page2/sign-up-page2.component.ts
+++ src/app/tya/User-landing-Page/sign-up-page2/sign-up-page2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-page2',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage2Component implements OnInit {
 
-  constructor() { }
+  signUpForm2:FormGroup;
+  constructor(private formBuilder:FormBuilder, private router:Router) { }
 
   ngOnInit(): void {
-  }
+  
+
+  this.signUpForm2=this.formBuilder.group({
+    firstName:['',Validators.required],
+    lastName:['',Validators.required],
+    companyName:['',Validators.required],
+    emailAdd:['',Validators.required],
+    password:['',Validators.required],
+    phoneNo:['',Validators.required],
+    agreement:['',Validators.required],
+    recEmail:['',Validators.required]
+  });
+}
 
 }
